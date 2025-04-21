114cc5985d807269ff4c4ada71161faa5f7ec7fb
Abhishekpsolera2917 <abhishek.pandey1@solera.com>
2025-04-21 19:52:46 +0530

    Spring core

--- SpringContext/src/main/java/org/example/annotations/context/componentScan/config/BeanConfig.java
+++ SpringContext/src/main/java/org/example/annotations/context/componentScan/config/BeanConfig.java
@@ -1,8 +1,11 @@
 package org.example.annotations.context.componentScan.config;
 
+import jakarta.annotation.;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.;
 
+@Order
 @Configuration
 @ComponentScan("org.example.annotations.context.componentScan")
 public class BeanConfig {
